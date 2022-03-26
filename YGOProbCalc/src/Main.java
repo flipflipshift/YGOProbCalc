0e559e9515337382f65d159361d08fe0504df088
cbtod <78685166+cbtod@users.noreply.github.com>
2022-03-26 07:35:01 -0400

    update demos, fix hand_size

--- YGOProbCalc/src/Main.java
+++ YGOProbCalc/src/Main.java
@@ -36,24 +36,27 @@ public class Main {
 		action("Patch eff").poss(move(patch,1,4), move(poly,0,1), move("edge",0,1)).hopt().open();	
 		action("Goods eff").poss(move(goods,1,4), move(vend,0,4)).poss(move(goods,1,4), move(repair,0,4)).hopt().open();
 		action("Repair gy eff").poss(move(repair,4,5), move("fluff",1,2));
+		
 		Action vend_gyeff = action("Vend gy effect").poss(move("fluff",0,1)).poss(move(sabres,0,1));
 		MT.add(vend, list(0,1,3), 4, vend_gyeff);
-		action("Vendor field eff").poss(cond(vend,3), move("card",1,4)).open().hopt();
-		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
-		action("Place Vend").poss(move(vend,1,3)).open();		
-		Action chain_eff = action("Chain eff").poss(move("frightfur",0,1)).hopt();
-		MT.add(chain, list(1,2), 4, chain_eff);		
-		action("normal").poss(move("ns",1,2)).open().hopt();
 		Action owl_effect=action("Owl effect").poss(move(poly,0,1)).hopt();
 		MT.add(owl, 1, 2, owl_effect);
 		Action dog_effect = action("Dog effect").poss(move("fluff",0,1).exclude(dog)).poss(move(sabres,0,1)).hopt();
 		MT.add(dog,1, 2, dog_effect);
+		action("normal").poss(move("ns",1,2)).open().hopt();
+		action("Gardens field eff").poss(cond(gardens,3), cond(peng,list(1,2)), move("card", 1, 4)).open().hopt();
+		Action chain_eff = action("Chain eff").poss(move("frightfur",0,1)).hopt();
+		MT.add(chain, list(1,2), 4, chain_eff);	
+		action("Vendor primitive field eff").poss(cond(vend,3), move("card",1,4)).open().hopt();
+		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
+		action("Place Vend").poss(move(vend,1,3)).open();	
 		
 		Gov.poss(cond("fluff", list(1,2)), cond("edge", list(1,2)), cond(poly, 1));	
 		go("Fluffal Demo 1", 100);
 	
 		
 	}
+
 	
 	public static void main2(String args[])
 	{
@@ -94,6 +97,7 @@ public class Main {
 		Action vend_gyeff = action("Vend gy effect").poss(move("fluff",0,1)).poss(move(sabres,0,1));
 		MT.add(vend, list(0,1,3), 4, vend_gyeff);
 		Action vend_field = action("Vendor field eff").poss(cond(vend,3), move("card",1,4)).open().hopt();
+		action("Gardens field eff").poss(cond(gardens,3), cond(peng,list(1,2)), move("card", 1, 4)).open().hopt();
 		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
 		action("Place Vend").poss(move(vend,1,3)).open();
 		Action chain_eff = action("Chain eff").poss(move("frightfur",0,1)).hopt();
@@ -104,8 +108,12 @@ public class Main {
 		Action dog_effect = action("Dog effect").poss(move("fluff",0,1).exclude(dog)).poss(move(sabres,0,1)).hopt();
 		MT.add(dog,1, 2, dog_effect);
 		
+		action("Vendor primitive field eff").poss(cond(vend,3), move("card",1,4)).open().hopt();
+		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
+		action("Place Vend").poss(move(vend,1,3)).open();	
+		
 		action("Wings eff").poss(move(wings,4,5), move("fluff",4,5), move(vend,3,4)).open().hopt().draw(1,2);
-		action("Activate Desires").poss(move(desires,1,4)).draw(5,10).draw(1,2).open().hopt();
+		action("Activate Desires").poss(move(desires,1,4)).draw(6,10).draw(1,2).open().hopt();
 		action("Special Souls").poss(move(souls,1,2), move(ioc,0,4)).open().hopt();
 		Action souls_1=action("souls for 1").poss(cond(souls,2),move("s/t", list(1,3),4)).draw(1,1).open().hopt();
 		Action souls_2=action("souls for 2").poss(cond(souls,2),move(2,"s/t", list(1,3),4)).draw(1,2).open().hopt();
@@ -118,8 +126,10 @@ public class Main {
 		Sabres_vend.turnoff(IOC_vend, vend_field);
 		IOC_vend.turnoff(Sabres_vend, vend_field);
 		vend_field.turnoff(Sabres_vend, IOC_vend);
-		Action fuse = action("Fuse into Whale").poss(move(poly,1,4), move("fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2));
-		fuse.poss(move(ff,1,4), move("fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2)).open();
+		Action fuse = action("Fuse into Whale").poss(move(poly,1,4), move("fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2)).open();
+		fuse.poss(move(ff,1,4), move("fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2));
+		fuse.poss(move(ff,1,4), move(2, "fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2));
+		fuse.poss(move(poly,1,4), move(2, "fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2));
 		Action fusion_process = action("Fusion processing").move_all(7, 4);
 		fuse.trigger(fusion_process);
 		Action penguin_discard = action("Peng disard").poss(move("card",1,4));
@@ -134,8 +144,9 @@ public class Main {
 		Gov.timeout(5);
 		go("Fluffal Demo 2", 100);
 	}
+		
 	
-	public static void main3(String args[])
+	public static void main(String args[])
 	{
 		Card dog = card("Fluffal Dog", 3, "fluff", "ns").small();
 		Card peng = card("Fluffal Penguin", 2, "fluff", "ns").small();
@@ -166,60 +177,65 @@ public class Main {
 		locations("Deck", "Hand", "MZ", "STZ", "GY", "Banish", "fdBanish", "Fusion Processing", "Vend Exc", "Prosp Exc");
 		hand(5);
 		
-		setsmallworld(small, 4, 6);
 		
-		action("Gardens eff").guarantee_poss(move(gardens,1,3),move(peng,0,1)).hopt().open();
-		action("Patch eff").guarantee_poss(move(patch,1,4), move(poly,0,1), move("edge",0,1)).hopt().open();
-		action("Goods eff").guarantee_poss(move(goods,1,4), move(vend,0,4)).poss(move(goods,1,4), move(repair,0,4)).hopt().open();
-		action("Repair gy eff").poss(move(repair,4,5), move("fluff",1,2));
+		
 		Action vend_gyeff = action("Vend gy effect").poss(move("fluff",0,1)).poss(move(sabres,0,1));
 		MT.add(vend, list(-1,0,1,3), 4, vend_gyeff);
-		Action vend_field=action("Vendor field eff").poss(move(vend,3,-1),move(vend_dead,-1,3), move("card",1,4)).open().draw(8, 1);
-		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
-		action("Place Vend").poss(move(vend,1,3)).open();		
 		Action chain_eff = action("Chain eff").poss(move("frightfur",0,1)).hopt();
-		MT.add(chain, list(1,2), 4, chain_eff);		
-		action("normal").poss(move("ns",1,2)).open().hopt();
+		MT.add(chain, list(1,2), 4, chain_eff);
 		Action owl_effect=action("Owl effect").poss(move(poly,0,1)).hopt();
 		MT.add(owl, 1, 2, owl_effect);
 		Action dog_effect = action("Dog effect").poss(move("fluff",0,1).exclude(dog)).poss(move(sabres,0,1)).hopt();
 		MT.add(dog,1, 2, dog_effect);
-		
-		Action wings_eff = action("Wings eff").poss(move(wings,4,5), move("fluff",4,5), move("vend",3,4)).open().hopt().draw(1,2);
-		Action desires_eff=action("Activate Desires").poss(move(desires,1,4)).draw(5,10).draw(1,2).open().hopt();
-		action("Special Souls").poss(move(souls,1,2), move(ioc,0,4)).open().hopt();
-		Action souls_1=action("souls for 1").poss(cond(souls,2),move("s/t", list(1,3),4)).draw(1,1).open().hopt();
-		Action souls_2=action("souls for 2").poss(cond(souls,2),move(2,"s/t", list(1,3),4)).draw(1,2).open().hopt();
-		souls_1.turnoff(souls_2);
-		souls_2.turnoff(souls_1);
-		action("IOC search").poss(move(ioc,1,0),move(souls,0,1)).open().hopt();
-		action("Sabres Special").poss(move(sabres,4,2), move("card", 1,0)).open().hopt();		
-		Action IOC_vend = action("IOC Vendor Combo").poss(move(vend,3,-1), move(vend_dead,-1,3), move(ioc,1,4),move("fluff",1,2), move(souls,0,1)).open().hopt();
-		Action Sabres_vend = action("Sabres Vendor Combo").poss(move(vend,3,-1), move(vend_dead,-1,3),move(sabres,4,2), move("fluff",1,2), move("card",1,4)).open().hopt();		
-		Action fuse = action("Fuse into Whale").poss(move(poly,1,4), move("fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2));
-		fuse.poss(move(ff,1,4), move("fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2)).open();
-		Action fusion_process = action("Fusion processing").move_all(7, 4);
-		fuse.trigger(fusion_process);
 		Action penguin_discard = action("Peng disard").poss(move("card",1,4));		
 		Action penguin_draw = action("Penguin eff").no_conditions().draw(1, 2).trigger(penguin_discard).hopt();		
 		Action cat_eff = action("Cat eff").poss(move(poly,4,1)).hopt();		
 		MT.add(cat, 7, 4, cat_eff);
-		MT.add(peng, 7, 4, penguin_draw);	
-		action("whale eff").poss(cond(whale,2), move(repair,0,4)).open().hopt();		
-		action("Make VFD").poss(move(2,whale,2,4), move(VFD,-1,2)).open().hopt();
-		
+		MT.add(peng, 7, 4, penguin_draw);
+		Action fusion_process = action("Fusion processing").move_all(7, 4);
+		Action dump_all =action("Allure dump hand").move_all(1, 4);
+		Action allure_payment = action("Allure Payment").poss(move("dark",1,5)).poss(dump_all);
 		Action vend_swapback = action("Swap back vendor").poss(move(vend_dead,4,-1), move(vend,-1,4)).poss(move(vend_dead,1,-1), move(vend,-1,1));
 		MT.add(vend_dead, 3, list(1,4), vend_swapback);		
 		Action vend_hit = action("Vendor Hit!").poss(move("ns",1,2)).poss(move("fluff",1,2));
-		Action vend_processing=action("Vendor Processing").poss(vend_hit,move("fluff",8,1)).poss(move("card",8,4));
-		vend_field.trigger(vend_processing);		
+		Action vend_processing=action("Vendor Processing").poss(vend_hit,move("fluff",8,1)).poss(move("card",8,4));		
 		Action prosp_return = action("Return to deck").move_all(9, 0);
 		Action prosp_add=action("Add off Prosperity").poss(move("card",9,1)).trigger(prosp_return);
+		
+		action("Gardens eff").guarantee_poss(move(gardens,1,3),move(peng,0,1)).hopt().open();
+		action("Patch eff").guarantee_poss(move(patch,1,4), move(poly,0,1), move("edge",0,1)).hopt().open();
+		action("Goods eff").guarantee_poss(move(goods,1,4), move(vend,0,4)).poss(move(goods,1,4), move(repair,0,4)).hopt().open();
+		action("Make VFD").guarantee_poss(move(2,whale,2,4), move(VFD,-1,2)).open().hopt();
+		Action fuse = action("Fuse into Whale").poss(move(poly,1,4), move("fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2));
+		fuse.poss(move(ff,1,4), move("fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2)).open();
+		fuse.poss(move(ff,1,4), move(2, "fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2));
+		fuse.poss(move(poly,1,4), move(2, "fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2));
+		fuse.trigger(fusion_process);
+		Action wings_eff = action("Wings eff").poss(move(wings,4,5), move("fluff",4,5), move("vend",3,4)).open().hopt().draw(1,2);
+		action("whale eff").poss(cond(whale,2), move(repair,0,4)).open().hopt();
+		action("Special Souls").poss(move(souls,1,2), move(ioc,0,4)).open().hopt();
+		Action IOC_vend = action("IOC Vendor Combo").poss(move(vend,3,-1), move(vend_dead,-1,3), move(ioc,1,4),move("fluff",1,2), move(souls,0,1)).open().hopt();
+		Action Sabres_vend = action("Sabres Vendor Combo").poss(move(vend,3,-1), move(vend_dead,-1,3),move(sabres,4,2), move("fluff",1,2), move("card",1,4)).open().hopt();		
+		action("IOC search").poss(move(ioc,1,0),move(souls,0,1)).open().hopt();
+		action("Repair gy eff").poss(move(repair,4,5), move("fluff",1,2));
+		action("normal").poss(move("ns",1,2)).open().hopt();
+		action("Place Vend").poss(move(vend,1,3)).open();
+		Action vend_field=action("Vendor field eff").poss(move(vend,3,-1),move(vend_dead,-1,3), move("card",1,4)).open().draw(8, 1);
+		vend_field.trigger(vend_processing);
+		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
+		Action souls_1=action("souls for 1").poss(cond(souls,2),move("s/t", list(1,3),4)).draw(1,1).open().hopt();
+		Action souls_2=action("souls for 2").poss(cond(souls,2),move(2,"s/t", list(1,3),4)).draw(1,2).open().hopt();
+		souls_1.turnoff(souls_2);
+		souls_2.turnoff(souls_1);
+		action("Sabres Special").poss(move(sabres,4,2), move("card", 1,0)).open().hopt();
+		setsmallworld(small, 4, 6);
+		action("Gardens field eff").poss(cond(gardens,3), cond(peng,list(1,2)), move("card", 1, 4)).open().hopt();
+		Action desires_eff=action("Activate Desires").poss(move(desires,1,4)).draw(6,10).draw(1,2).open().hopt();
+		action("Allure eff").poss(move(allure,1,4)).draw(1,2).open().trigger(allure_payment);
 		Action prosp_eff=action("Prosperity eff").poss(move(prosp,1,4)).open().hopt().draw(9,6).trigger(prosp_add);		
-		Action dump_all =action("Allure dump hand").move_all(1, 4);
-		Action allure_payment = action("Allure Payment").poss(move("dark",1,5)).poss(dump_all);
-		action("Allure eff").poss(move(allure,1,4)).draw(1,2).open().trigger(allure_payment);	
-		prosp_eff.turnoff(vend_field,IOC_vend,Sabres_vend,wings_eff,souls_1, souls_2, penguin_draw);
+
+		//also bear's field eff that turns off
+		prosp_eff.turnoff(vend_field,IOC_vend,Sabres_vend,wings_eff,souls_1, souls_2, penguin_draw, desires_eff);
 		vend_field.turnoff(prosp_eff);
 		IOC_vend.turnoff(prosp_eff);
 		Sabres_vend.turnoff(prosp_eff);
@@ -227,12 +243,15 @@ public class Main {
 		souls_1.turnoff(prosp_eff);
 		souls_2.turnoff(prosp_eff);
 		penguin_draw.turnoff(prosp_eff);
+		desires_eff.turnoff(prosp_eff);
 		
 		
 		Gov.poss(cond(VFD,2));
 		Gov.timeout(5);
-		go("Fluffal Demo 3", 1000);
+		go("Fluffal Demo 3", 100);
 	}
+	
+
 	
 	
 	
@@ -346,7 +365,7 @@ public class Main {
 	}
 	private static void hand(int hand_size)
 	{
-		Gov.hand_size=5;
+		Gov.hand_size=hand_size;
 	}
 	private static void locations(String... locations)
 	{
