beb963e12bed4054401030e67516fd17bba896e8
cbtod <78685166+cbtod@users.noreply.github.com>
2022-04-16 12:28:05 -0400

    Removed main methods for testing

--- YGOProbCalc/src/Main.java
+++ YGOProbCalc/src/Main.java
@@ -2,58 +2,7 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class Main {
-	public static void mainTest(String args[])
-	{
-		
-		Card tuner1 = card("Tuner lv 1", 3, "tuner").numerics(1,1);
-		Card tuner2 = card("Tuner lv 2", 3, "tuner").numerics(2,1);
-		Card tuner3 = card("Tuner lv 3", 3, "tuner").numerics(3,1);
-		Card tuner4 = card("Tuner lv 4", 3, "tuner").numerics(4,1);
-		Card nontuner1 = card("Non-Tuner lv 1", 3, "non-tuner").numerics(1,1);
-		Card nontuner2 = card("Non-Tuner lv 2", 3, "non-tuner").numerics(2,1);
-		Card nontuner3 = card("Non-Tuner lv 3", 3, "non-tuner").numerics(3,1);
-		Card nontuner4 = card("Non-Tuner lv 4", 3, "non-tuner").numerics(4,1);
-		
-		Card sync4 = card("Synchro lv 4", 0, "non-tuner").numerics(4, 1);
-		Card sync5 = card("Synchro lv 5", 0, "non-tuner").numerics(5,1);
-		Card sync6 = card("Synchro lv 6", 0, "non-tuner").numerics(6,1);
-		Card sync7 = card("Synchro lv 7", 0, "non-tuner").numerics(7,1);
-		Card sync8 = card("Synchro lv 8", 0, "non-tuner").numerics(8,1);
-		Card sync9 = card("Synchro lv 9", 0, "non-tuner").numerics(9,1);
-		Card sync10 = card("Synchro lv 10", 0, "non-tuner").numerics(10,1);
-		
-		Card link2 = card("Link 2", 0).numerics(0,2);
-		Card link3 = card("Link 3", 0).numerics(0,3);
-		
-		locations("Deck", "Hand", "Field", "GY");
-		
-		action("Unlimited normal").move_all(1,2).open().hopt();
-		action("Synchro 4").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync4,-1,2)).sum(4,2)).open();
-		action("Synchro 4").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync4,-1,2)).sum(4,2)).open();
-		action("Synchro 5").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync5,-1,2)).sum(5, 2)).open();
-		action("Synchro 5").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync5,-1,2)).sum(5, 2)).open();
-		action("Synchro 6").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync6,-1,2)).sum(6, 2)).open();
-		action("Synchro 6").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync6,-1,2)).sum(6, 2)).open();
-		action("Synchro 7").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync7,-1,2)).sum(7, 2)).open();
-		action("Synchro 7").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync7,-1,2)).sum(7, 2)).open();
-		action("Synchro 8").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync8,-1,2)).sum(8, 2)).open();
-		action("Synchro 8").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync8,-1,2)).sum(8, 2)).open();
-		action("Synchro 9").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync9,-1,2)).sum(9, 2)).open();
-		action("Synchro 9").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync9,-1,2)).sum(9, 2)).open();
-		action("Synchro 10").add_poss(new Possibility(move(1, "tuner",2,3), move(1,"non-tuner",2,3), move(sync10,-1,2)).sum(10, 2)).open();
-		action("Synchro 10").add_poss(new Possibility(move(1, "tuner",2,3), move(2,"non-tuner",2,3), move(sync10,-1,2)).sum(10, 2)).open();
-		
-		action("Link 2 Summon").add_poss(new Possibility(move(2, "card", 2, 3), move(link2, -1,2)).sum(1,2,1)).open();
-		action("Link 3 Summon").add_poss(new Possibility(move(2, "card", 2, 3), move(link3, -1,2)).sum(1,3,1)).open();
-		
-		
-//		Gov.poss(cond(sync10, 2));
-		Gov.poss(cond(link3, 2));
-		go("Sum testing", 10);
-		
-		
-		
-	}
+	
 	
 	public static void main1(String args[])
 	{
@@ -107,93 +56,7 @@ public class Main {
 		
 	}
 	
-	public static void main(String args[])
-	{
-		Card dog = card("Fluffal Dog", 3, "fluff", "ns").small();
-		Card peng = card("Fluffal Penguin", 2, "fluff", "ns").small();
-		Card cat = card("Fluffal Cat", 1, "fluff").small();
-		Card owl = card("Fluffal Owl", 1, "fluff", "ns").small();
-		Card bear = card("Fluffal Bear", 2, "fluff").small();
-		Card wings = card("Fluffal Wings", 2, "fluff").small();
-		Card chain = card("Edge Imp Chain", 3, "edge").small();
-		Card sabres = card("Edge Imp Sabres", 1, "edge").small();
-		Card souls = card("Magicians' Souls", 2).small();
-		Card ioc = card("Illusion of Chaos", 2).small();
-		Card patch = card("Patchwork", 3, "s/t", "frightfur");
-		Card ff = card("Frightfur Fu", 1, "s/t", "frightfur");
-		Card repair = card("Frightfur rep", 1, "s/t", "frightfur");
-		Card poly = card("Poly", 3, "s/t");
-		Card small = card("Small World", 2, "s/t");
-		Card vend = card("Toy Vendor", 3, "s/t");
-		Card goods = card("Foolish goods", 3, "s/t");
-		Card gardens = card("Peng Gardens", 2, "s/t");
-		Card desires = card("Desires", 1, "s/t");
-		Card prosp = card("Prosp", 1, "s/t");
-		Card allure = card("Allure", 1, "s/t");
-		
-		Card whale = card("FF Whale", 0);
-		Card VFD = card("VFD", 0);
-		
-		locations("Deck", "Hand", "MZ", "STZ", "GY", "Banish", "fdBanish", "Fusion Processing");
-		hand(5);
-		
-		setsmallworld(small, 4, 6);
-		
-		action("Gardens eff").poss(costMove(gardens,1,3),move(peng,0,1)).hopt().open().interruptable("Ash");
-		action("Patch eff").poss(costMove(patch,1,4), move(poly,0,1), move("edge",0,1)).hopt().open().interruptable("Ash");
-		action("Goods eff").poss(move(goods,1,4), move(vend,0,4)).poss(move(goods,1,4), move(repair,0,4)).hopt().open();
-		action("Repair gy eff").poss(move(repair,4,5), move("fluff",1,2));
-		Action vend_gyeff = action("Vend gy effect").poss(move("fluff",0,1)).poss(move(sabres,0,1)).interruptable("Ash");
-		MT.add(vend, list(0,1,3), 4, vend_gyeff);
-		Action vend_field = action("Vendor field eff").poss(cond(vend,3), costMove("card",1,4)).open().hopt();//.interruptable("Ash");
-		action("Gardens field eff").poss(cond(gardens,3), cond(peng,list(1,2)), move("card", 1, 4)).open().hopt();
-		action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
-		action("Place Vend").poss(move(vend,1,3)).open();
-		Action chain_eff = action("Chain eff").poss(move("frightfur",0,1)).hopt().interruptable("Ash");
-		MT.add(chain, list(1,2), 4, chain_eff);
-		action("normal").poss(move("ns",1,2)).open().hopt();
-		Action owl_effect=action("Owl effect").poss(move(poly,0,1)).hopt().interruptable("Ash", "Veiler");
-		MT.add(owl, 1, 2, owl_effect);
-		Action dog_effect = action("Dog effect").poss(move("fluff",0,1).exclude(dog)).poss(move(sabres,0,1)).hopt().interruptable("Ash", "Veiler");
-		MT.add(dog,1, 2, dog_effect);
-		
-//		action("Vendor primitive field eff").poss(cond(vend,3), costMove("card",1,4)).open().hopt().interruptable("Ash");
-		//action("Bear eff").poss(move(bear,1,4), move(vend,0,3)).open().hopt();
-		//action("Place Vend").poss(move(vend,1,3)).open();	
-		
-		action("Wings eff").poss(costMove(wings,4,5), move("fluff",4,5), move(vend,3,4)).open().hopt().draw(1,2).interruptable("Ash");
-		action("Activate Desires").poss(costMove(desires,1,4)).draw(6,10).draw(1,2).open().hopt().interruptable("Ash");
-		action("Special Souls").poss(move(souls,1,2), move(ioc,0,4)).open().hopt();
-		Action souls_1=action("souls for 1").poss(cond(souls,2),costMove("s/t", list(1,3),4)).draw(1,1).open().hopt().interruptable("Ash", "Veiler");
-		Action souls_2=action("souls for 2").poss(cond(souls,2),costMove(2,"s/t", list(1,3),4)).draw(1,2).open().hopt().interruptable("Ash", "Veiler");
-		souls_1.turnoff(souls_2);
-		souls_2.turnoff(souls_1);
-		action("IOC search").poss(move(ioc,1,0),move(souls,0,1)).open().hopt();
-		action("Sabres Special").poss(move(sabres,4,2), move("card", 1,0)).open().hopt();
-		Action IOC_vend = action("IOC Vendor Combo").poss(cond(vend,3), move(ioc,1,4),move("fluff",1,2), move(souls,0,1)).open().hopt();
-		Action Sabres_vend = action("Sabres Vendor Combo").poss(cond(vend,3), move(sabres,4,2), move("fluff",1,2), move("card",1,4)).open().hopt();
-		Sabres_vend.turnoff(IOC_vend, vend_field);
-		IOC_vend.turnoff(Sabres_vend, vend_field);
-		vend_field.turnoff(Sabres_vend, IOC_vend);
-		Action fuse = action("Fuse into Whale").poss(move(poly,1,4), move("fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2)).open();
-		fuse.poss(move(ff,1,4), move("fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2));
-		fuse.poss(move(ff,1,4), move(2, "fluff", list(2,4), 5), move("edge", list(2,4), 5), move(whale,-1,2));
-		fuse.poss(move(poly,1,4), move(2, "fluff",list(1,2),7), move("edge", list(1,2),4),move(whale,-1,2));
-		Action fusion_process = action("Fusion processing").move_all(7, 4);
-		fuse.trigger(fusion_process);
-		Action penguin_discard = action("Peng disard").poss(move("card",1,4));
-		Action penguin_draw = action("Penguin eff").no_conditions().draw(1, 2).trigger(penguin_discard).hopt().interruptable("Ash");
-		Action cat_eff = action("Cat eff").poss(move(poly,4,1)).hopt();
-		MT.add(cat, 7, 4, cat_eff);
-		MT.add(peng, 7, 4, penguin_draw);
-		action("whale eff").poss(cond(whale,2), move(repair,0,4)).open().hopt();
-		action("Make VFD").poss(move(2,whale,2,4), move(VFD,-1,2)).open().hopt();
-		
-		Gov.interruptions("Ash", "Veiler");
-		Gov.poss(cond(VFD,2));
-		Gov.timeout(5);
-		go("Fluffal Demo 2", 10);
-	}
+	
 		
 	
 	public static void main3(String args[])
